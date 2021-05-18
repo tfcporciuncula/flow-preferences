7bb14c8f008d09453059970d24d2518f0c91217d
Fred Porciúncula <tfcporciuncula@gmail.com>
2021-05-18 11:33:30 +0200

    Update Kotlin and coroutines to 1.5.0
    No need for the offerCatching() workaround anymore since that issue has been fixed.

--- flow-preferences/src/main/java/com/tfcporciuncula/flow/SharedPreferencesExtensions.kt
+++ flow-preferences/src/main/java/com/tfcporciuncula/flow/SharedPreferencesExtensions.kt
@@ -2,7 +2,6 @@ package com.tfcporciuncula.flow
 
 import android.content.SharedPreferences
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
 
@@ -10,12 +9,7 @@ import kotlinx.coroutines.flow.callbackFlow
 val SharedPreferences.keyFlow
   get() = callbackFlow {
     // key can be null when preferences are cleared on Android R+
-    val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, key: String? -> offerCatching(key) }
+    val listener = SharedPreferences.OnSharedPreferenceChangeListener { _, key: String? -> trySend(key) }
     registerOnSharedPreferenceChangeListener(listener)
     awaitClose { unregisterOnSharedPreferenceChangeListener(listener) }
   }
-
-// https://github.com/Kotlin/kotlinx.coroutines/issues/974
-private fun <E> SendChannel<E>.offerCatching(element: E): Boolean {
-  return runCatching { offer(element) }.getOrDefault(false)
-}
