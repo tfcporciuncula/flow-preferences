8488c819aa2ccc8dceec1fc3d99e80197391733e
Jon Poulton <jpoulton@pm.me>
2023-10-15 11:12:40 +0100

    Add StringSerializer interfaces

--- flow-preferences/src/main/java/com/fredporciuncula/flow/preferences/Serializers.kt
+++ flow-preferences/src/main/java/com/fredporciuncula/flow/preferences/Serializers.kt
@@ -13,3 +13,15 @@ interface NullableSerializer<T> {
   fun deserialize(serialized: String?): T?
   fun serialize(value: T?): String?
 }
+
+/** Allows for the simple case where the class [T] is serialized to string using the [toString] method override. */
+fun interface StringSerializer<T : Any> : Serializer<T> {
+  override fun serialize(value: T): String = value.toString()
+}
+
+/**
+ * Allows for the simple case where the nullable class [T] is serialized to string using the [toString] method override.
+ */
+fun interface NullableStringSerializer<T : Any> : NullableSerializer<T> {
+  override fun serialize(value: T?): String? = value?.toString()
+}
